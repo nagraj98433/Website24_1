40654b18f1f87a77826e37eac61887a5833bc5f0
Nagraj Vade <rajkapool123@gmail.com>
2023-05-06 19:33:34 +0530

    form validation added

--- src/HomePage/Hire_yourself/Hire_yourself.jsx
+++ src/HomePage/Hire_yourself/Hire_yourself.jsx
@@ -9,6 +9,21 @@ const Hire_yourself = () => {
 
   const [index, setIndex] = useState(0);
 
+  const [name, setName] = useState('');
+  const [mobile, setMobile] = useState('');
+  const [formValidated, setFormValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+    }
+    setFormValidated(true);
+  };
+
+
+
 
   const handleSlideChange = (selectedIndex) => {
     setIndex(selectedIndex);
@@ -66,15 +81,23 @@ const Hire_yourself = () => {
           <div className="row">
             <div className="col-10">
               <div className='from_container'>
-                <form>
-                  <div class="mb-3">
-                    <input type="email" class="form-control HomePage_Form" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Enter Your Name' />
+
+                <form noValidate onSubmit={handleSubmit} className={formValidated ? 'was-validated' : ''}>
+                  <div className="mb-3">
+                    <input type="text" className="form-control HomePage_Form" id="exampleInputName" placeholder="Enter Your Name" required value={name} onChange={(e) => setName(e.target.value)} autoComplete='off' />
+                    <div className="invalid-feedback">Please enter your name</div>
                   </div>
-                  <div class="mb-3">
-                    <input type="password" class="form-control HomePage_Form" id="exampleInputPassword1" placeholder='Enter Mobile Number' />
+                  <div className="mb-3">
+                    <input type="tel" className="form-control HomePage_Form" id="exampleInputMobile" placeholder="Enter Mobile Number" pattern="[0-9]{10}" required value={mobile} onChange={(e) => setMobile(e.target.value)} autoComplete='off' />
+                    <div className="invalid-feedback">Please enter a valid 10-digit mobile number</div>
                   </div>
-                  <div className='text-center'> <button type="submit" class="btn btn-primary form_btn">SUBMIT</button></div>
+                  <div className="text-center">
+                    <button type="submit" className="btn btn-primary form_btn">SUBMIT</button>
+                  </div>
                 </form>
+
+
+
                 <div className='Horizontal_line mt-4'><hr /></div>
                 <div className='text-center'>
                   <div className='Available_services'>AVAILABLE SERVICES</div>
